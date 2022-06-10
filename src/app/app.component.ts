7169a35aeac2b1add0bf20e66decb6704964c197
thefelipe001 <61038817+thefelipe001@users.noreply.github.com>
2022-06-09 22:49:54 -0400

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import Swal from 'sweetalert2';
+import { ToastrService } from 'ngx-toastr';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'App-Airline';
+export class AppComponent implements OnInit {
+  name = 'Angular';
+  constructor(
+    private toastr: ToastrService) {
+  
+   }
+
+  ngOnInit() {
+    console.log('This is init method');
+  }
+
+  simpleAlert() {
+    Swal.fire('Hello world!');
+    this.toastr.success('Tarjeta Registrada!', 'La tarjeta fue Registrada Correctamente!');
+
+  }
+
+  alertWithSuccess() {
+    Swal.fire('Thank you...', 'You submitted succesfully!', 'success');
+  }
+
+  confirmBox() {
+    Swal.fire({
+      title: 'Are you sure want to remove?',
+      text: 'You will not be able to recover this file!',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonText: 'Yes, delete it!',
+      cancelButtonText: 'No, keep it',
+    }).then((result) => {
+      if (result.value) {
+        Swal.fire(
+          'Deleted!',
+          'Your imaginary file has been deleted.',
+          'success'
+        );
+      } else if (result.dismiss === Swal.DismissReason.cancel) {
+        Swal.fire('Cancelled', 'Your imaginary file is safe :)', 'error');
+      }
+    });
+  }
 }
